7bec70e00fa668e9c119839118a6fc1ec4af1ada
Corey Perrigo <corey@perrigogames.com>
2017-12-19 19:01:31 -0800

    Initial Commit

---
+++ CardProtoRenderer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CardProtoRenderer
+//
+//  Created by Corey Perrigo on 12/19/17.
+//  Copyright © 2017 Corey Perrigo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
